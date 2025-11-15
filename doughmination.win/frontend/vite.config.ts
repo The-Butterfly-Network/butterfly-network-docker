1619fd5e8a3b5ab7d0b4f36ca90edb1eb0d4594d
Clove Twilight <clovetwilight3@outlook.com>
2025-11-15 02:54:39 +0000

    okay undo all that

--- doughmination.win/frontend/vite.config.ts
+++ doughmination.win/frontend/vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig(({ mode }) => {
         },
         // Proxy avatar requests 
         '/avatars': {
-          target: 'http://127.0.0.1:8000/avatars',
+          target: 'http://127.0.0.1:8000',
           changeOrigin: true,
           secure: false,
         }
